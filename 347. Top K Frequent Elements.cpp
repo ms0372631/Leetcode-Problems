4b2d55e32105af3c4a4204a0e2184db3e62166eb
Alan Chiu <ms0372631@gmail.com>
2022-01-17 15:46:37 -0800

    leetcode 347

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,21 @@
+priority_queue<pair<int, int>> pq;
+unordered_map<int, int> freq;
+vector<int> res;
+
+for (int num : nums)
+  ++freq[num];
+
+for (auto a : freq)
+{
+  pq.push(make_pair(-a.second, a.first));
+  if (pq.size() > k)
+    pq.pop();
+}
+
+while (!pq.empty())
+{
+  res.push_back(pq.top().second);
+  pq.pop();
+}
+
+return res;

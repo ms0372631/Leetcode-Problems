5e01c10cd36e091f01dfe6070c3acde9cbae9cb1
Alan Chiu <ms0372631@gmail.com>
2022-01-20 09:51:13 -0800

    leetcode 287

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,17 @@
+int l = 0, r = nums.size() - 1;
+
+while (l < r)
+{
+  int mid = l + (r - l) / 2;
+  int cnt = 0;
+  for (auto num : nums)
+  {
+    if (num <= mid)
+      ++cnt;
+  }
+  if (cnt <= mid)
+    l = mid + 1;
+  else
+    r = mid;
+}
+return l;

8283e5cfc4ee34c964588b6c017147ae7e9324d9
Alan Chiu <ms0372631@gmail.com>
2022-01-31 21:44:13 -0800

    leetcode 523

--- 523. Continuous Subarray Sum.cpp
+++ 523. Continuous Subarray Sum.cpp
@@ -1,8 +1,7 @@
 unordered_set<int> s;
 int sum = 0, pre = 0;
 
-for (int i = 0; i < nums.size(); ++i)
-{
+for (int i = 0; i < nums.size(); ++i) {
   sum += nums[i];
   if (s.count(sum % k))
     return true;

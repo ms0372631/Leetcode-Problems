a321d252dec68dbd8ded4d80e5045dd1d9a0f0ac
Alan Chiu <ms0372631@gmail.com>
2022-03-03 17:51:38 -0800

    leetcode 162

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -1,8 +1,6 @@
-int findPeakElement(vector<int> &nums)
-{
+int findPeakElement(vector<int> &nums) {
   int l = 0, r = nums.size() - 1;
-  while (l < r)
-  {
+  while (l < r) {
     int mid = l + (r - l) / 2;
     if (nums[mid] < nums[mid + 1])
       l = mid + 1;
@@ -10,5 +8,4 @@ int findPeakElement(vector<int> &nums)
       r = mid;
   }
   return l;
-}
 };
